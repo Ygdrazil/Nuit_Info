28fe7d22db34ef3db7feffdf6e0174f7ab9261c5
tomfcz <37912558+tomfcz@users.noreply.github.com>
2021-12-02 23:53:17 +0100

    Update 2021_12_02_233915_all_updates.php

--- database/migrations/2021_12_02_233915_all_updates.php
+++ database/migrations/2021_12_02_233915_all_updates.php
@@ -13,43 +13,43 @@ class AllUpdates extends Migration
      */
     public function up()
     {
-        Schema::update('bateau_sauvetage', function (Blueprint $table) {
+        Schema::table('bateau_sauvetage', function (Blueprint $table) {
             $table->foreign("id_bateau")->references('id_bateau')->on('bateau');
         });
 
-        Schema::update('info_bateau', function (Blueprint $table) {
+        Schema::table('info_bateau', function (Blueprint $table) {
             $table->foreign("id_bateau")->references('id_bateau')->on('bateau');
         });
 
-        Schema::update('soumission', function (Blueprint $table) {
+        Schema::table('soumission', function (Blueprint $table) {
             $table->foreign("id_bateau")->references('id_bateau')->on('bateau')->nullable();
         });
 
-		Schema::update('bateau_sauvetage', function (Blueprint $table) {
+		Schema::table('bateau_sauvetage', function (Blueprint $table) {
 			$table->foreign("id_sauvetage")->references('id_sauvetage')->on('sauvetage');
 		});
 
-		Schema::update('participant', function (Blueprint $table) {
+		Schema::table('participant', function (Blueprint $table) {
 			$table->foreign("id_sauvetage")->references('id_sauvetage')->on('sauvetage');
 		});
 
-		Schema::update('participant', function (Blueprint $table) {
+		Schema::table('participant', function (Blueprint $table) {
 			$table->foreign("id_sauveteur")->references('id_sauveteur')->on('sauveteur')->nullable();
 		});
 
-		Schema::update('poste', function (Blueprint $table) {
+		Schema::table('poste', function (Blueprint $table) {
 			$table->foreign("id_sauveteur")->references('id_sauveteur')->on('sauveteur');
 		});
 
-		Schema::update('soumission', function (Blueprint $table) {
+		Schema::table('soumission', function (Blueprint $table) {
 			$table->foreign("id_sauveteur")->references('id_sauveteur')->on('sauveteur')->nullable();
 		});
 
-		Schema::update('soumission', function (Blueprint $table) {
+		Schema::table('soumission', function (Blueprint $table) {
             $table->foreign("email_user")->nullable()->references('users')->on('email');
         });
 
-		Schema::update('soumission', function (Blueprint $table) {
+		Schema::table('soumission', function (Blueprint $table) {
 			$table->foreign("id_sauvetage")->references('id_sauvetage')->on('sauvetage')->nullable();
 		});
     }
