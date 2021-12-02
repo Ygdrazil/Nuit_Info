e73b1c633a7d3790f52b97107ebc0a05e91045df
thomaspeissel <84765571+thomaspeissel@users.noreply.github.com>
2021-12-03 00:29:59 +0100

    Update head.blade.php

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<titlD>Sauveteurs du Dunkerquois</title>
+<title>Sauveteurs du Dunkerquois</title>
 
 <!-- STYLES -->
 <link rel="icon" href="{{ asset('pictures/boat.png') }}" />
