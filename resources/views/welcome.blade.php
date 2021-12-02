5bafeb5b95a032d3d9237c4f6aa784b1e9604544
thomaspeissel <84765571+thomaspeissel@users.noreply.github.com>
2021-12-02 21:29:18 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,10 +26,10 @@
                 <div id="wave4" class="wave"></div>
             </div>
             <ul class="social_icon">
-                <li><a href="#"><ion-icon name="home-outline"></ion-icon></a></li>
-                <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
-                <li><a href="#"><ion-icon name="logo-twitter"></ion-icon></a></li>
-                <li><a href="#"><ion-icon name="mail-outline"></ion-icon></a></li>
+                <li><a href="."><ion-icon name="home-outline"></ion-icon></a></li>
+                <li><a href="https://www.facebook.com/groups/938396409644949"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                <li><a href="https://twitter.com/boutelierphili1"><ion-icon name="logo-twitter"></ion-icon></a></li>
+                <li><a href="mailto:sauveteurdudunkerquois@gmail.com"><ion-icon name="mail-outline"></ion-icon></a></li>
             </ul>
             <ul class="menu">
                 <li><a href="#">Qui sommes-nous</a></li>
