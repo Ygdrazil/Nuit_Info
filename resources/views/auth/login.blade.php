189e031448cc9fdf312909ec811c9f3ad6b0b061
tomfcz <37912558+tomfcz@users.noreply.github.com>
2021-12-02 21:57:40 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,6 @@
 <form action="{{ route('login.post') }}" method="post">
+	@csrf
+	
 	<input type="text" name="email" id="email" value="{{ old('email') }}" />
 	<input type="password" name="password" id="password" value="{{ old('password') }}" />
 
