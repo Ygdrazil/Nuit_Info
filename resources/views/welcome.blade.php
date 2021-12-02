9de05d418f739663e7ada6f1bfa5865dc2368b14
thomaspeissel <84765571+thomaspeissel@users.noreply.github.com>
2021-12-02 21:25:33 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Bateau</title>
+        <title>Accueil ⋆ Sauveteurs du dunkerquois</title>
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/misc.css') }}">
