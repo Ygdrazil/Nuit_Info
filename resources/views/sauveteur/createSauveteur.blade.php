239da3488124826304765e06f0eb30910d3d2423
LuxemTheFez <37304100+LuxemTheFez@users.noreply.github.com>
2021-12-03 00:23:51 +0100

    je suis turbo con

--- resources/views/sauveteur/createSauveteur.blade.php
+++ resources/views/sauveteur/createSauveteur.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        @include('layouts.head')
     </head>
     <body>
         @include('layouts.header')
-        @include('layouts.head')
 
         <br>
         <br>
