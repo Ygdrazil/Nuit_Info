13d92400b8667a4927827dd16a371575a4dfd46d
thomaspeissel <84765571+thomaspeissel@users.noreply.github.com>
2021-12-02 22:38:56 +0100

    css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
         <!-- UP -->
         <div id="up">
             <div class="rocket">
-                <img src="{{ asset('pictures/rocket.png') }}">
+                <img class="img_rocket" src="{{ asset('pictures/rocket.png') }}">
             </div>
         </div>
 
