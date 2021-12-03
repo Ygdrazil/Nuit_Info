fa1a567b395ca1277845323199ff904b22edb7a7
Tan <antoined925@gmail.com>
2021-12-03 03:51:30 +0100

    Update easter.js

--- public/js/easter.js
+++ public/js/easter.js
@@ -11,6 +11,7 @@ window.addEventListener("DOMContentLoaded", _ => {
     var click =0
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
     const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+    var notStart = true
 
     logo.addEventListener("click", (event) => {
         click+=1
@@ -19,28 +20,28 @@ window.addEventListener("DOMContentLoaded", _ => {
     document.addEventListener("keypress", (event) => {
         var keyName = event.key;
         if(click >= 3){
-            if(parseInt(logo.style.top <= vh)){
+            if(parseInt(logo.style.top <= vh) || notStart){
                 if (keyName == "s") {
                     logo.style.top = (parseInt(logo.style.top || 40) + 10)+'px';
                 };
             }
-            if(parseInt(logo.style.left >= 0)){
+            if(parseInt(logo.style.left >= 0) || notStart){
                 if (keyName == "q") {
                     logo.style.left = (parseInt(logo.style.left || 30) - 10)+'px';
                     console.log(click)
                 };
             }
-            if(parseInt(logo.style.left <= vw)){
+            if(parseInt(logo.style.left <= vw) || notStart){
                 if (keyName == "d") {
                     logo.style.left = (parseInt(logo.style.left || 30) + 10)+'px';
                 };
             }
-            if(parseInt(logo.style.top >= 0)){
+            if(parseInt(logo.style.top >= 0) || notStart){
                 if (keyName == "z") {
                     logo.style.top = (parseInt(logo.style.top || 40) - 10)+'px';
                 };
             }
-            console.log(click)
+            notStart = false
         }
     }, false);
 });
