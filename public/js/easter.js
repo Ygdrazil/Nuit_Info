f9496f896ace8736b13f01c0fc8982ab1d7b56dc
Tan <antoined925@gmail.com>
2021-12-03 03:43:09 +0100

    easter

--- public/js/easter.js
+++ public/js/easter.js
@@ -8,19 +8,36 @@ window.addEventListener("DOMContentLoaded", _ => {
     logo.style.position = 'absolute'
     contentLogo.appendChild(logo)
 
-    document.addEventListener("keypress", (event) => {
-        var keyName = event.key;
-        if (keyName == "s") {
-            logo.style.top = (parseInt(logo.style.top || 40) + 10)+'px';
-        };
-        if (keyName == "q") {
-            logo.style.left = (parseInt(logo.style.left || 30) - 10)+'px';
-        };
-        if (keyName == "d") {
-            logo.style.left = (parseInt(logo.style.left || 30) + 10)+'px';
-        };
-        if (keyName == "z") {
-            logo.style.top = (parseInt(logo.style.top || 40) - 10)+'px';
-        };
-    }, false);
+    var click =0
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
+    logo.addEventListener("click", (event) => {
+        click+=1
+        if (click>=3){
+            document.addEventListener("keypress", (event) => {
+                var keyName = event.key;
+                if(parseInt(logo.style.top <= vh)){
+                    if (keyName == "s") {
+                        logo.style.top = (parseInt(logo.style.top || 40) + 10)+'px';
+                    };
+                }
+                if(parseInt(logo.style.left >= 0)){
+                    if (keyName == "q") {
+                        logo.style.left = (parseInt(logo.style.left || 30) - 10)+'px';
+                    };
+                }
+                if(parseInt(logo.style.left <= vw)){
+                    if (keyName == "d") {
+                        logo.style.left = (parseInt(logo.style.left || 30) + 10)+'px';
+                    };
+                }
+                if(parseInt(logo.style.top >= 0)){
+                    if (keyName == "z") {
+                        logo.style.top = (parseInt(logo.style.top || 40) - 10)+'px';
+                    };
+                }
+            }, false);
+        }
+    });
 });
