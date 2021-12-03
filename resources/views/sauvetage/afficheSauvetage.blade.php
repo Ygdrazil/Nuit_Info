951aa4cb5d4b4e8085cb6d80fed58662edbe61ad
LuxemTheFez <37304100+LuxemTheFez@users.noreply.github.com>
2021-12-03 06:46:24 +0100

    Update afficheSauvetage.blade.php

--- resources/views/sauvetage/afficheSauvetage.blade.php
+++ resources/views/sauvetage/afficheSauvetage.blade.php
@@ -17,16 +17,17 @@
 		<!-- MAIN -->
 		<section id="main">
 			@JSON($sauvetage)
+			@auth
+				<form action="{{ route('soumission.nouvelle.post') }}" method="post">
+					@csrf
+					<h2>Faire une soumission</h2>
+					<textarea name="description"></textarea>
 
-			<form action="{{ route('soumission.nouvelle.post') }}" method="post">
-				@csrf
-				<h2>Faire une soumission</h2>
-				<textarea name="description"></textarea>
-
-				<input type="hidden" name="id" value="{{ $sauvetage->id_sauvetage }}">
-				<input type="hidden" name="type" value="id_sauvetage">
-				<button type="submit">Envoyer</button>
-			</form>
+					<input type="hidden" name="id" value="{{ $sauvetage->id_sauvetage }}">
+					<input type="hidden" name="type" value="id_sauvetage">
+					<button type="submit">Envoyer</button>
+				</form>
+			@endauth
 		</section>
 
 		<!-- FOOTER -->
