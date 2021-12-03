7fa4c615a829f2655fd70fcd812d7354b0e367da
Tan <antoined925@gmail.com>
2021-12-03 02:05:11 +0100

    easter

--- public/js/easter.js
+++ public/js/easter.js
@@ -1,3 +1,4 @@
 window.addEventListener("DOMContentLoaded", _ => {
-    logo = document.querySelectorAll('logo')[0].pointer-events= 'none';
+    logo = document.querySelectorAll('logo a img')[0];
+    //document.querySelectorAll('logo')[0].style.display = 'none'
 });
