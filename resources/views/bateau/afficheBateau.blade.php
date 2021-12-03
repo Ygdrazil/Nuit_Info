ca63e567ffec2e819622855f933940455bd6fdf6
thomaspeissel <84765571+thomaspeissel@users.noreply.github.com>
2021-12-03 01:25:02 +0100

    css

---
+++ resources/views/bateau/afficheBateau.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+		@include('layouts.head')
+
+		<!-- Styles -->
+
+		<!-- Scripts -->
+
+		<!-- Libs -->
+
+    </head>
+    <body>
+		<!-- HEADER -->
+		@include('layouts.header')
+
+		<!-- MAIN -->
+		<section id="main">
+			<p>Salut mec test</p>
+		</section>
+
+		<!-- FOOTER -->
+    	@include('layouts.footer')
+    </body>
+</html>
+
+
---
+++ resources/views/sauvetage/afficheSauvetage.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+		@include('layouts.head')
+
+		<!-- Styles -->
+
+		<!-- Scripts -->
+
+		<!-- Libs -->
+
+    </head>
+    <body>
+		<!-- HEADER -->
+		@include('layouts.header')
+
+		<!-- MAIN -->
+		<section id="main">
+			
+		</section>
+
+		<!-- FOOTER -->
+    	@include('layouts.footer')
+    </body>
+</html>
+
+
