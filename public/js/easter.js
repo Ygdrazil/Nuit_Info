fc29c91ef101ee930aa7985ae27db0316a495108
Tan <antoined925@gmail.com>
2021-12-03 04:40:16 +0100

    Update easter.js

--- public/js/easter.js
+++ public/js/easter.js
@@ -2,6 +2,7 @@ window.addEventListener("DOMContentLoaded", _ => {
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
     const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
+    //Création de la div du bateau pour l'easter egg
     logo = document.querySelectorAll('.logo a img')[0];
     contentLogo = document.querySelectorAll('.logo')[0];
     document.querySelectorAll('.logo a')[0].style.display = 'none'
@@ -11,13 +12,14 @@ window.addEventListener("DOMContentLoaded", _ => {
     logo.style.position = 'absolute'
     contentLogo.appendChild(logo)
 
+    //Création de la div du phare
     var img = document.createElement('img');
     img.src = 'pictures/phare.png';
     contentPhare = document.querySelectorAll('.nav-content')[0];
     img.style.height = '30px'
     img.style.width = 'auto'
-    img.style.top = '55px'
-    img.style.left = '55px'
+    img.style.top = '15px'
+    img.style.right = '85px'
     img.style.position = 'absolute'
     contentPhare.appendChild(img)
 
@@ -28,6 +30,7 @@ window.addEventListener("DOMContentLoaded", _ => {
         click+=1
     });
 
+    //Event de déplacement, il faut toucher le bateau au moins 3 fois pour le lancer
     document.addEventListener("keypress", (event) => {
         var keyName = event.key;
         if(click >= 3){
@@ -59,4 +62,20 @@ window.addEventListener("DOMContentLoaded", _ => {
             }
         }
     }, false);
+
+    let x1 = logo.offset().left;
+    let y1 = logo.offset().top;
+    let h1 = logo.outerHeight(true);
+    let w1 = logo.outerWidth(true);
+    let b1 = y1 + h1;
+    let r1 = x1 + w1;
+    let x2 = phare.offset().left;
+    let y2 = phare.offset().top;
+    let h2 = phare.outerHeight(true);
+    let w2 = phare.outerWidth(true);
+    let b2 = y2 + h2;
+    let r2 = x2 + w2;
+
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2)
+        document.location.href="."
 });
