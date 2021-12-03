af489c4b1a2b4ad21e605c2a0b80d63a006cedbd
tomfcz <37912558+tomfcz@users.noreply.github.com>
2021-12-03 03:27:09 +0100

    Update SauvetageController.php

--- app/Http/Controllers/SauvetageController.php
+++ app/Http/Controllers/SauvetageController.php
@@ -42,8 +42,9 @@ class SauvetageController extends Controller
     public function findAll()
     {
         $sauvetages = Sauvetage::orderBy('date_sauvetage')->get();
+		$min = $sauvetages[0];
 
-		dd($sauvetages);
+		dd($min);
         return view('sauvetage.afficheSauvetages', compact('sauvetages'));
     }
 
