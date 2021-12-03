15ad312c0e93e7e45dcb1e42b76830c0d35af6c9
Tan <antoined925@gmail.com>
2021-12-03 03:48:19 +0100

    Update easter.js

--- public/js/easter.js
+++ public/js/easter.js
@@ -18,25 +18,32 @@ window.addEventListener("DOMContentLoaded", _ => {
 
     document.addEventListener("keypress", (event) => {
         var keyName = event.key;
-        if(parseInt(logo.style.top <= vh)){
-            if (keyName == "s") {
-                logo.style.top = (parseInt(logo.style.top || 40) + 10)+'px';
-            };
-        }
-        if(parseInt(logo.style.left >= 0)){
-            if (keyName == "q") {
-                logo.style.left = (parseInt(logo.style.left || 30) - 10)+'px';
-            };
-        }
-        if(parseInt(logo.style.left <= vw)){
-            if (keyName == "d") {
-                logo.style.left = (parseInt(logo.style.left || 30) + 10)+'px';
-            };
-        }
-        if(parseInt(logo.style.top >= 0)){
-            if (keyName == "z") {
-                logo.style.top = (parseInt(logo.style.top || 40) - 10)+'px';
-            };
+        if(click >= 3){
+            if(parseInt(logo.style.top <= vh)){
+                if (keyName == "s") {
+                    logo.style.top = (parseInt(logo.style.top || 40) + 10)+'px';
+                };
+                console.log(click)
+            }
+            if(parseInt(logo.style.left >= 0)){
+                if (keyName == "q") {
+                    logo.style.left = (parseInt(logo.style.left || 30) - 10)+'px';
+                };
+                console.log(click)
+            }
+            if(parseInt(logo.style.left <= vw)){
+                if (keyName == "d") {
+                    logo.style.left = (parseInt(logo.style.left || 30) + 10)+'px';
+                };
+                console.log(click)
+            }
+            if(parseInt(logo.style.top >= 0)){
+                if (keyName == "z") {
+                    logo.style.top = (parseInt(logo.style.top || 40) - 10)+'px';
+                };
+                console.log(click)
+            }
+            console.log(click)
         }
     }, false);
 });
