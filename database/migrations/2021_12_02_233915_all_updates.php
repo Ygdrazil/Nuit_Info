36d4399e99b3dc212c19ebd96af75c98869d50fd
tomfcz <37912558+tomfcz@users.noreply.github.com>
2021-12-03 05:52:15 +0100

    Update 2021_12_02_233915_all_updates.php

--- database/migrations/2021_12_02_233915_all_updates.php
+++ database/migrations/2021_12_02_233915_all_updates.php
@@ -13,23 +13,23 @@ class AllUpdates extends Migration
      */
     public function up()
     {
-        // Schema::table('bateau_sauvetage', function (Blueprint $table) {
-        //     $table->foreignId("id_bateau")->references('id_bateau')->on('bateau');
-		// 	$table->foreignId("id_sauvetage")->references('id_sauvetage')->on('sauvetage');
-        // });
+        Schema::table('bateau_sauvetage', function (Blueprint $table) {
+            $table->foreignId("id_bateau")->references('id_bateau')->on('bateau');
+			$table->foreignId("id_sauvetage")->references('id_sauvetage')->on('sauvetage');
+        });
 
-        // Schema::table('info_bateau', function (Blueprint $table) {
-        //     $table->foreignId("id_bateau")->references('id_bateau')->on('bateau');
-        // });
+        Schema::table('info_bateau', function (Blueprint $table) {
+            $table->foreignId("id_bateau")->references('id_bateau')->on('bateau');
+        });
 
-		// Schema::table('participant', function (Blueprint $table) {
-		// 	$table->foreignId("id_sauvetage")->references('id_sauvetage')->on('sauvetage');
-		// 	$table->foreignId("id_sauveteur")->references('id_sauveteur')->on('sauveteur')->nullable();
-		// });
+		Schema::table('participant', function (Blueprint $table) {
+			$table->foreignId("id_sauvetage")->references('id_sauvetage')->on('sauvetage');
+			$table->foreignId("id_sauveteur")->references('id_sauveteur')->on('sauveteur')->nullable();
+		});
 
-		// Schema::table('poste', function (Blueprint $table) {
-		// 	$table->foreignId("id_sauveteur")->references('id_sauveteur')->on('sauveteur');
-		// });
+		Schema::table('poste', function (Blueprint $table) {
+			$table->foreignId("id_sauveteur")->references('id_sauveteur')->on('sauveteur');
+		});
 
 		Schema::table('soumission', function (Blueprint $table) {
 			$table->foreignId("id_bateau")->references('id_bateau')->on('bateau')->nullable();
