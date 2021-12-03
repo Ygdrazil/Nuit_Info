e321ddc1d6403b894c709e1502704919b02496e1
tomfcz <37912558+tomfcz@users.noreply.github.com>
2021-12-03 02:45:41 +0100

    frh

--- resources/views/sauvetage/createSauvetage.blade.php
+++ resources/views/sauvetage/createSauvetage.blade.php
@@ -1,1 +1,38 @@
-xxxx
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @include('layouts.head')
+
+        <!-- Styles -->
+
+        <!-- Scripts -->
+
+        <!-- Libs -->
+        
+    </head>
+    <body>
+        <!-- HEADER -->
+        @include('layouts.header')
+
+        <!-- MAIN -->
+        <section id="main">
+			<form action="{{ route('sauveteur.nouveau.post') }}" method="POST">
+				@csrf
+				<input type="text" name="titre" placeholder="Titre">
+                <input type="date" name="date_sauvetage">
+                <textarea name="description" placeholder="Description" >
+        
+                </textarea>
+                <textarea name="source" placeholder="Source" maxlength="120">
+                </textarea>
+                <input type="text" name="info_sauvetage" placeholder="Info Sauvetage">
+                <input type="number" name="nb_sauve" placeholder="nb_sauve">
+                <input type="time" name="duree">
+                <input type="submit" value="Créer un sauvetage">
+			</form>
+        </section>
+
+        <!-- FOOTER -->
+    	@include('layouts.footer')
+    </body>
+</html>
