4e9462cc803b74713c652ebf9ea2f44b0d401dc9
thomaspeissel <84765571+thomaspeissel@users.noreply.github.com>
2021-12-03 03:09:30 +0100

    css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,7 @@
 
 		<!-- MAIN -->
 		<section id="main">
+      <h1 class="line anim-typewriter">Typewriter effect using css()</h1>
       <p>Bienvenue sur le site des sauveteurs du dunkerquois. Ce site rend hommage aux femmes, hommes et enfants qui ont réalisé des actes de sauvetages en milieu aquatique.</p>
       <p>Ces sauveteurs, habitants du dunkerquois (de Bray-Dunes à Grand-Fort-Philippe), ont participé à plus de 900 sauvetages en mer et plus de 1100 sauvetages individuels. Œuvrant avec courage, abnégation et souvent au mépris du risque ils méritent amplement que leurs actes soient pérennisés.</p>
       <br>
