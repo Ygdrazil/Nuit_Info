8b7a73dcf2673663db29691347fcadc832128e78
Tan <antoined925@gmail.com>
2021-12-03 02:20:31 +0100

    Update easter.js

--- public/js/easter.js
+++ public/js/easter.js
@@ -2,5 +2,7 @@ window.addEventListener("DOMContentLoaded", _ => {
     logo = document.querySelectorAll('.logo a img')[0];
     contentLogo = document.querySelectorAll('.logo')[0];
     document.querySelectorAll('.logo a')[0].style.display = 'none'
+    logo.style.height = '30px'
+    logo.style.width = 'auto'
     contentLogo.appendChild(logo)
 });
