1823b4695a726e561981fe37529d5a3c48464025
Louis-Bernot <louisbernot.pro@gmail.com>
2021-12-03 02:40:39 +0100

    Update entry.js

--- public/js/entry.js
+++ public/js/entry.js
@@ -1,8 +1,8 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
 const username = document.getElementById('username');
-// const name = document.getElementById('name');
-// const surname = document.getElementById('surname');
+const namee = document.getElementById('name');
+const surname = document.getElementById('surname');
 const password = document.getElementById('password');
 const confirm_password = document.getElementById('confirm_password');
 
@@ -14,35 +14,55 @@ form.addEventListener('submit', e => {
 
 function checkInputs() {
 	// trim to remove the whitespaces
-	const usernameValue = username.value.trim();
 	const emailValue = email.value.trim();
+    const usernameValue = username.value.trim();
+	const nameValue = namee.value.trim();
+    const surnameValue = surname.value.trim();
 	const passwordValue = password.value.trim();
 	const confirm_passwordValue = confirm_password.value.trim();
 	
+    //email
+    if(emailValue === '') {
+        setErrorFor(email, 'Veuillez renseigner un email');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'L\'email n\'est pas valide');
+    } else {
+        setSuccessFor(email);
+    }
+    
+    //username
 	if(usernameValue === '') {
-		setErrorFor(username, 'Username cannot be blank');
+		setErrorFor(username, 'Veuillez renseigner un nom d\'utilisateur');
+	} else {
+		setSuccessFor(username);
+	}
+
+    //name
+    if(nameValue === '') {
+		setErrorFor(username, 'Veuillez renseigner votre prénom');
+	} else {
+		setSuccessFor(username);
+	}
+
+    //surname
+    if(surnameValue === '') {
+		setErrorFor(username, 'Veuillez renseigner votre nom');
 	} else {
 		setSuccessFor(username);
 	}
 	
-	if(emailValue === '') {
-		setErrorFor(email, 'Email cannot be blank');
-	} else if (!isEmail(emailValue)) {
-		setErrorFor(email, 'Not a valid email');
-	} else {
-		setSuccessFor(email);
-	}
-	
+    //password
 	if(passwordValue === '') {
-		setErrorFor(password, 'Password cannot be blank');
+		setErrorFor(password, 'Veuillez renseigner un nom mot de passe');
 	} else {
 		setSuccessFor(password);
 	}
 	
+    //confirm password
 	if(confirm_passwordValue === '') {
-		setErrorFor(confirm_password, 'confirm_password cannot be blank');
+		setErrorFor(confirm_password, 'Veuillez confirmer le mot de passe');
 	} else if(passwordValue !== confirm_passwordValue) {
-		setErrorFor(confirm_password, 'Passwords does not match');
+		setErrorFor(confirm_password, 'Les mots de passe ne correspondent pas');
 	} else{
 		setSuccessFor(confirm_password);
 	}
@@ -59,7 +79,7 @@ function setSuccessFor(input) {
 	const formControl = input.parentElement;
 	formControl.className = 'form-control success';
 }
-	
+
 function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
